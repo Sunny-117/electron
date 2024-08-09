6c03424fdff02365fed82c1d5c3142bbafa81454
Sunny-117 <zhiqiangfu6@gmail.com>
2024-08-09 18:32:06 +0800

    macOS

---
+++ packages/打包macOS应用/index.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require("electron");
+require("./menu");
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // 允许在渲染进程里面使用 Node.js
+      contextIsolation: false, // 关闭上下文隔离
+    },
+  });
+
+  return win;
+};
+
+app.whenReady().then(() => {
+  const win = createWindow();
+  win.loadFile("./window/index.html");
+});
