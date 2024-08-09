e8763ea5e8c70b62f2c12a701a2c533a03c3a216
Sunny-117 <zhiqiangfu6@gmail.com>
2024-08-09 15:58:50 +0800

    chore: fix package name

---
+++ packages/os/index.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require("electron");
+
+// 创建窗口的方法
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true, // 允许在渲染进程（在窗口）里面使用 node.js
+      contextIsolation: false, // 关闭上下文隔离
+    },
+  });
+  win.loadFile("index.html");
+};
+
+// whenReady是一个生命周期方法，会在 Electron 完成应用初始化后调用
+// 返回一个 promise
+app.whenReady().then(() => {
+  createWindow();
+});
